a68761ee02082bc7e8d1ecb68e282b682c06ac66
Muhammad Saad Mateen <saad.5690@gmail.com>
2021-01-09 03:49:26 +0500

    Create a blog using MDX

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
 
 export default () => {
+  const posts = usePosts();
+
   return (
     <Layout>
       <h1>Home</h1>
       <p>Hello Berlin!</p>
       <Link to="/about/">Learn about me &rarr;</Link>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post}></PostPreview>
+      ))}
     </Layout>
   );
 };
