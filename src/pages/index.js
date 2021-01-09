81d41c97fcd9d6480f02addbaa60c41f734d50cd
Muhammad Saad Mateen <saad.5690@gmail.com>
2021-01-09 05:42:06 +0500

    Create homepage Hero Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import Hero from '../components/hero';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
 
@@ -8,15 +8,14 @@ export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Berlin!</p>
-      <Link to="/about/">Learn about me &rarr;</Link>
-
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post}></PostPreview>
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post}></PostPreview>
+        ))}
+      </Layout>
+    </>
   );
 };
